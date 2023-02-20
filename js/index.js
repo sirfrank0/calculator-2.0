415770daf6a590c738a02407622b58101f40ba7c
SirFrank0 <frankseb04@gmail.com>
2023-02-20 15:14:58 -0500

    add Display.js

--- js/index.js
+++ js/index.js
@@ -5,3 +5,9 @@ const displayValorActual = document.getElementById('valor-actual');
 const botonesNumeros = document.querySelectorAll('.numero');
 const botonesOperadores = document.querySelectorAll('.operador'); 
 
+const calculadora = new Calculadora();
+
+console.log(calculadora.sumar(2,3));
+console.log(calculadora.restar(2,3));
+console.log(calculadora.multiplicar(2,3));
+console.log(calculadora.dividir(2,3));
